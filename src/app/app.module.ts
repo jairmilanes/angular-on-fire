82554918d45d5358f622e50b39247ef9cf85738a
JMilanes <weblayoutz@gmail.com>
2019-08-09 09:57:37 -0600

    documentation update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightModule } from 'ngx-highlightjs';
+
+import bash from 'highlight.js/lib/languages/bash';
+import typescript from 'highlight.js/lib/languages/typescript';
+
+export function hljsLanguages() {
+  return [
+    {name: 'typescript', func: typescript},
+    {name: 'bash', func: bash}
+  ];
+}
+
 
 @NgModule({
   declarations: [
@@ -10,7 +22,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    HighlightModule.forRoot({
+      languages: hljsLanguages
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
