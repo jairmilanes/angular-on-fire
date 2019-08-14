f5aae1de221f363105ab2f4e4f2f917c57eb0f8e
JMilanes <weblayoutz@gmail.com>
2019-08-13 21:48:34 -0600

    updated cloud function to use configuration based region settings

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
-import {https} from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import {environment} from '../../src/environments/environment';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,8 +7,14 @@ import {https} from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+const {firebase} = environment;
 
+if (!firebase.region || !(firebase.region in functions.SUPPORTED_REGIONS)) {
+    throw new Error('The firebase region must be set in the base environment file.');
+}
+
+const region: any = firebase.region;
+const {https} = functions.region(region);
 const universal = require(`${process.cwd()}/dist/index`);
+
 export const ssrServer = https.onRequest(universal.server);
-
-
