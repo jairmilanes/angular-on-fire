82dfcb9bc99b1a396885e01da644459e15da1a04
JMilanes <weblayoutz@gmail.com>
2019-08-13 21:51:26 -0600

    updated gulp copy task to first clear the functions/dist on every build

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 const { src, dest } = require('gulp');
+const del = require('del')
 
 function help() {
     console.log('Help Gulp');
 }
 
+/**
+ * Copies the current build from `./dist` tot the `./functions/dist` folder
+ * so it can be deployed to cloud functions
+ *
+ * @return {Promise}
+ */
 function copyBuildToFunctions() {
-    return src('dist/**').pipe(dest('functions/dist/'));
+    del.sync('functions/dist/**', {force: true});
+
+    return src('dist/**')
+        .pipe(dest('functions/dist/'));
 }
 
 exports.default  = help;
